6ba90cc315a55bbb4483f558afc15f40af85b3c5
omarissajbour <221148@ppu.edu.ps>
2025-07-19 18:59:16 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/students-json-api/',
+  base: '/students-json-api/', 
   plugins: [react()],
 })
