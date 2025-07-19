443addea2ab955713bb3ee85c6da6d20bf948f0f
omarissajbour <221148@ppu.edu.ps>
2025-07-19 18:49:11 +0300

    final version sub22222mit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/students-management-app/',
+  base: '/students-json-api/',
   plugins: [react()],
 })
